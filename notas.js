4c9abf53cbde9d8b5357106bd953cefad033fea8
jorgedavidbatistahernandez-lang <jorgedavidbatistahernandez@gmail.com>
2026-05-26 00:42:07 -0400

    Update notas.js

--- notas.js
+++ notas.js
@@ -22,7 +22,7 @@ const evaluaciones = [
         Esta en el grupo de WhataApp :D
 `,
         notas: {
-            5: ["Daniel Zaldívar Figueredo","Victor Manuel Maceira Mir","Jesús Hernández Guio", "Julio Alejandro Gámez Calzadilla", "Yadiel Jorge Acosta Ricardo"]
+            5: ["Daniel Zaldívar Figueredo","Victor Manuel Maceira Mir","Jesús Hernández Guio", "Julio Alejandro Gámez Calzadilla", "Yadiel Jorge Acosta Ricardo",]
             4: ["Randy Ernesto Suárez Rodríguez","",""]
         }
     },
